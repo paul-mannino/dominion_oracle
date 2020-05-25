7bf97cd401f348d89730eb6d51f236e586d328c0
Paul Mannino <pmannino.code@gmail.com>
2020-05-24 23:36:19 -0500

    Fix failing test & add additional tests

--- tests/test_simulation.py
+++ tests/test_simulation.py
@@ -2,13 +2,27 @@ from dominion_oracle.simulation import Simulation
 from dominion_oracle.cards import create_card
 
 
+def assert_close(expected, actual, tolerance=0.05):
+    assert abs(expected - actual) <= tolerance
+
+
 def test_expected_value_of_starting_deck():
     card_list = [create_card(name) for name in ["estate"] * 3 + ["copper"] * 7]
-    simulation = Simulation(card_list)
-    assert abs(simulation.expected_terminal_value() - 3.5) < 0.05
+    assert_close(3.5, Simulation(card_list).expected_terminal_value())
 
 
 def test_expected_value_of_deck_with_draw():
     cards = ["copper"] * 7 + ["estate"] * 3 + ["gold", "smithy", "village"]
     simulation = Simulation([create_card(name) for name in cards])
-    assert abs(simulation.expected_terminal_value() - 5.3) < 0.05
+    assert_close(5.205, simulation.expected_terminal_value())
+
+
+def test_expected_value_of_deck_with_no_value():
+    simulation = Simulation(["duchy", "smithy", "village", "laboratory"])
+    assert_close(0, simulation.expected_terminal_value())
+
+
+def test_expected_value_of_treasure_only_deck():
+    cards = ["copper"] * 10 + ["silver"] * 3 + ["gold"] * 2
+    expected_result = 5 * ((10 + 3 * 2 + 2 * 3) / 15)
+    assert_close(expected_result, Simulation(cards).expected_terminal_value())
